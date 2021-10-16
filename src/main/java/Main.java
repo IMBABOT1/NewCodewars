40d93aceee9fe5bd6acd632b08f6d5a4128b0e7d
IMBABOT1 <zlovred12@gmail.com>
2021-10-16 22:41:14 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static Double multiply(Double a, Double b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(multiply(1.0, 2.0));
+    }
+}
