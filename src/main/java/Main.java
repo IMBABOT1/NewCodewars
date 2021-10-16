ed973ae814fd24ce74f7be0c9886d6e7a186e1b8
IMBABOT1 <zlovred12@gmail.com>
2021-10-16 22:50:51 +0300

    temp

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,4 @@
 public class Main {
 
-    public static Double multiply(Double a, Double b) {
-        return a * b;
-    }
 
-    public static void main(String[] args) {
-        System.out.println(multiply(1.0, 2.0));
-    }
 }
