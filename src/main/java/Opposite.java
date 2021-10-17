f8f24b66826a407831ba17a3b736fbb7b261a948
IMBABOT1 <zlovred12@gmail.com>
2021-10-17 20:35:15 +0300

    Opposite

---
+++ src/main/java/Opposite.java
@@ -0,0 +1,6 @@
+public class Opposite {
+    public static int opposite(int number) {
+        return number * -1;
+    }
+
+}
--- src/main/java/Persist.java
+++ src/main/java/Persist.java
@@ -2,44 +2,37 @@ import java.util.Arrays;
 
 public class Persist {
     public static int persistence(long n) {
+        amount(n);
+        return 1;
+    }
+
+    private static long amount(long n) {
         String temp = String.valueOf(n);
         String[] arr = temp.split("");
         long[] numbers = new long[arr.length];
-
-        while (amount(numbers) > 1) {
-            res = multiply(numbers);
-            amount(res);
-        }
-
-        System.out.println(result);
-
-        return result;
-    }
-
-    private static long multiply(long[] n){
+        int amount = 0;
         long result = 1;
-        for (long element : n){
-            result *= element;
-        }
-        return result;
-    }
-
-    private static int amount(long[] n){
-        String temp = String.valueOf(n);
-        String[] arr = temp.split("");
-        long[] numbers = new long[arr.length];
-        int result = 0;
 
 
         for (int i = 0; i < arr.length; i++) {
             numbers[i] = Integer.valueOf(arr[i]);
         }
 
-
-        for (int i = 0; i < n.length ; i++) {
-            result++;
+        for (int i = 0; i < numbers.length; i++) {
+            amount++;
         }
 
+
+        if (amount >= 2) {
+            for (long element : numbers) {
+                result *= element;
+                amount(result);
+            }
+        }
+
+        System.out.println(result);
+
         return result;
+
     }
 }
