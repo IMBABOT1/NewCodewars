bceadc1ea4b5da4f5f2c317431cd62c7e7cc991d
IMBABOT1 <zlovred12@gmail.com>
2021-10-17 12:23:58 +0300

    temp

--- src/main/java/Persist.java
+++ src/main/java/Persist.java
@@ -4,14 +4,42 @@ public class Persist {
     public static int persistence(long n) {
         String temp = String.valueOf(n);
         String[] arr = temp.split("");
-        int[] numbers = new int[arr.length];
+        long[] numbers = new long[arr.length];
+
+        while (amount(numbers) > 1) {
+            res = multiply(numbers);
+            amount(res);
+        }
+
+        System.out.println(result);
+
+        return result;
+    }
+
+    private static long multiply(long[] n){
+        long result = 1;
+        for (long element : n){
+            result *= element;
+        }
+        return result;
+    }
+
+    private static int amount(long[] n){
+        String temp = String.valueOf(n);
+        String[] arr = temp.split("");
+        long[] numbers = new long[arr.length];
+        int result = 0;
+
 
         for (int i = 0; i < arr.length; i++) {
             numbers[i] = Integer.valueOf(arr[i]);
         }
 
-        System.out.println(Arrays.toString(numbers));
 
-        return 1;
+        for (int i = 0; i < n.length ; i++) {
+            result++;
+        }
+
+        return result;
     }
 }
