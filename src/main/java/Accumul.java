8ac206f4b1d1f239914d81ed0697d5a1dfc9a700
IMBABOT1 <zlovred12@gmail.com>
2021-11-20 18:20:02 +0300

    Accumul

--- src/main/java/Accumul.java
+++ src/main/java/Accumul.java
@@ -1,19 +1,42 @@
 import java.util.Arrays;
+import java.util.Locale;
 
 public class Accumul {
 
     public static String accum(String s) {
         String[] temp = s.split("");
         StringBuilder sb = new StringBuilder();
+        StringBuilder res = new StringBuilder();
 
         int counter = 0;
         while (counter < temp.length) {
             for (int i = -1; i < counter ; i++) {
-                System.out.println(temp[counter]);
+                sb.append(temp[counter]);
             }
+            sb.append("-");
             counter++;
         }
-        return "";
+
+        String result = sb.toString().toLowerCase(Locale.ROOT);
+        String[] arr = result.split("");
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i].equals("-")){
+               arr[i+1] = arr[i+1].replaceAll(arr[i+1], arr[i+1].toUpperCase(Locale.ROOT));
+            }
+        }
+        arr[0] = arr[0].toUpperCase(Locale.ROOT);
+
+
+        for (int i = 0; i < arr.length ; i++) {
+            if (!arr[i].equals(",")){
+                res.append(arr[i]);
+            }
+        }
+
+
+        res.setLength(res.length() - 1);
+        return res.toString();
     }
 }
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
 
     public static void main(String[] args) {
-        Accumul.accum("RqaEzty");
+        System.out.println(Accumul.accum("RqaEzty"));
 
 
     }
