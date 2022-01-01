89f58479f29e69d535d02d110e3785c0560548f9
IMBABOT1 <zlovred12@gmail.com>
2022-01-01 12:29:59 +0300

    BreakCamelCase

--- src/main/java/BreakCamelCase.java
+++ src/main/java/BreakCamelCase.java
@@ -3,29 +3,25 @@ import java.util.Arrays;
 public class BreakCamelCase {
 
     public static String camelCase(String input) {
-        int divider = 0;
+        String result = "";
+        boolean bool = false;
+
         for (int i = 0; i < input.length() ; i++) {
-            if (Character.isUpperCase(input.charAt(i))){
-                divider = i;
+            if (Character.isUpperCase(input.charAt(i))) {
+                bool = true;
             }
         }
 
-        String[] arr = input.split("");
-        String[] first = new String[divider];
-        String[] second = new String[input.length() - divider];
+        if (bool){
+            result = input.replaceAll("(.)([A-Z])", "$1 $2");
+        }else {
+            result = input;
+        }
 
-        System.arraycopy(arr, 0, first, 0, divider);
-        System.arraycopy(arr, divider, second, 0, divider + 1);
-
-        System.out.println(Arrays.toString(first));
-
-        System.out.println(Arrays.toString(second));
-
-
-        return null;
+        return result;
     }
 
     public static void main(String[] args) {
-        camelCase("camelCasing");
+        camelCase("camelCasingTest");
     }
 }
